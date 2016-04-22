c4359ee7b00bd6b4d18860ebd0071fc160952a70
Thomas Versteeg <thomasversteeg@gmx.com>
2016-04-22 13:04:32 +0200

    Setup demo file for ccore and reimplement more functions

--- demo/ccore/main.c
+++ demo/ccore/main.c
@@ -1,11 +1,55 @@
+#include <stdio.h>
+
 #define MC_IMPLEMENTATION
 #define MC_OUTPUT_TEXTURE_RGB
 #include "../../micronsole.h"
 
+#include <ccore/display.h>
+#include <ccore/window.h>
+#include <ccore/opengl.h>
+#include <ccore/time.h>
+#include <ccore/file.h>
+
+#define WIDTH 100
+#define HEIGHT 100
+
+void mc_test_command(struct mc_console *con, int argc, char **argv)
+{
+	printf("Command\n");
+}
+
 int main(void)
 {
+	ccDisplayInitialize();
+	ccWindowCreate((ccRect){0, 0, WIDTH, HEIGHT}, "microconsole ccore demo", 0);
+
 	struct mc_console con;
 	mc_create(&con);
+	mc_set_texture_size(&con, WIDTH, HEIGHT);
+	mc_map(&con, "test", &mc_test_command);
+
+	bool loop = true;
+	while(loop){
+		while(ccWindowEventPoll()){
+			ccEvent event = ccWindowEventGet();
+			switch(event.type){
+				case CC_EVENT_WINDOW_QUIT:
+					loop = false;
+					break;
+				case CC_EVENT_KEY_DOWN:
+					if(event.keyCode == CC_KEY_ESCAPE){
+						loop = false;
+					}
+					break;
+				default: break;
+			}
+		}
+
+		ccTimeDelay(6);
+	}
+
+	mc_free(&con);
+	ccFree();
 
 	return 0;
 }
--- micronsole.h
+++ micronsole.h
@@ -7,7 +7,13 @@ DEFINES:
 MC_OUTPUT_TEXTURE_[RGB, RGBA, BGR, BGRA] - render the output as a texture with the defined pixel format
 MC_DYNAMIC_ARRAYS - dynamically grow the array size instead of using static sizes
 MC_MAX_COMMANDS (n>0) - maximum number of commands that can be registered, only useable when MC_DYNAMIC_ARRAYS is not defined
+MC_MAX_COMMAND_LENGTH (n>0) - maximum string length of the command, only useable when MC_DYNAMIC_ARRAYS is not defined
 MC_MAX_INPUT_LENGTH (n>0) - maximum length of the input string, only useable when MC_DYNAMIC_ARRAYS is not defined
+MC_ASSERT - define the assert function, leave empty for no assertions
+
+TODO:
+UTF8 support
+Option to replace stdlib
 
 LICENSE:
 This software is dual-licensed to the public domain and under the following
@@ -17,6 +23,11 @@ publish and distribute this file as you see fit.
 
 #ifndef MC_H
 #define MC_H
+
+#include <stdbool.h>
+#include <stdlib.h>
+#include <string.h>
+#include <assert.h>
 
 #if defined MC_OUTPUT_TEXTURE_RGB || defined MC_OUTPUT_RGBA || defined MC_OUTPUT_BGR || defined MC_OUTPUT_BGRA
 #define MC_OUTPUT_TEXTURE
@@ -30,11 +41,18 @@ publish and distribute this file as you see fit.
 #define MC_MAX_INPUT_LENGTH 256
 #endif
 
-#include <stdbool.h>
-#include <string.h>
+#ifndef MC_MAX_COMMAND_LENGTH
+#define MC_MAX_COMMAND_LENGTH 64
+#endif
+
+#ifndef MC_ASSERT
+#define MC_ASSERT(x) assert(x)
+#endif
+
+enum mc_keys {MC_KEY_LEFT, MC_KEY_RIGHT, MC_KEY_UP, MC_KEY_DOWN, MC_KEY_BACKSPACE};
 
 #ifdef MC_OUTPUT_TEXTURE
-struct {
+struct mc_pixel {
 #ifdef MC_OUTPUT_TEXTURE_RGB
 	unsigned char r, g, b;
 #elif defined MC_OUTPUT_TEXTURE_RGBA
@@ -46,7 +64,7 @@ struct {
 #else
 #error Undefined pixel format for texture output
 #endif
-} mc_pixel;
+};
 #endif
 
 typedef struct mc_console _mc_console_t;
@@ -69,12 +87,12 @@ struct mc_console {
 
 #ifdef MC_DYNAMIC_ARRAYS
 	mc_cmd_ptr *cmdfuncs;
-	char *cmds;
+	char **cmds;
 #else
 	mc_cmd_ptr cmdfuncs[MC_MAX_COMMANDS];
-	char *cmds[MC_MAX_COMMANDS];
+	char cmds[MC_MAX_COMMANDS][MC_MAX_COMMAND_LENGTH];
 #endif
-	unsigned ncommands;
+	unsigned ncmds;
 
 #ifdef MC_OUTPUT_TEXTURE
 	unsigned width, height;
@@ -87,6 +105,9 @@ void mc_free(struct mc_console *con);
 
 void mc_map(struct mc_console *con, const char *cmd, mc_cmd_ptr func);
 
+void mc_input_key(struct mc_console *con, enum mc_keys key);
+void mc_input_char(struct mc_console *con, char key);
+
 #ifdef MC_OUTPUT_TEXTURE
 void mc_set_texture_size(struct mc_console *con, unsigned width, unsigned height);
 #endif
@@ -95,15 +116,111 @@ void mc_set_texture_size(struct mc_console *con, unsigned width, unsigned height
 
 void mc_create(struct mc_console *con)
 {
+	MC_ASSERT(con);
 	memset(con, 0, sizeof(struct mc_console));
-	
-#ifndef MC_DYNAMIC_ARRAYS
-	memset(con->instr, 0, MC_MAX_INPUT_LENGTH);
+
+#ifdef MC_DYNAMIC_ARRAYS
+	con->instr = (char*)calloc(1, sizeof(char));
+	con->outstr = (char*)calloc(1, sizeof(char));
+#else
+	memset(con->instr, '\0', MC_MAX_INPUT_LENGTH * sizeof(char));
 	memset(con->cmdfuncs, 0, MC_MAX_COMMANDS * sizeof(mc_cmd_ptr));
-	memset(con->cmds, 0, MC_MAX_COMMANDS);
+	for(int i = 0; i <  MC_MAX_COMMANDS; i++){
+		memset(con->cmds[i], '\0', MC_MAX_COMMAND_LENGTH * sizeof(char));
+	}
 #endif
 }
 
+void mc_free(struct mc_console *con)
+{
+	MC_ASSERT(con);
+#ifdef MC_DYNAMIC_ARRAYS
+	free(con->instr);
+	free(con->cmdfuncs);
+	free(con->cmds);
+#endif
+}
+
+void mc_map(struct mc_console *con, const char *cmd, mc_cmd_ptr func)
+{
+	MC_ASSERT(con);
+#ifdef MC_DYNAMIC_ARRAYS
+	con->cmdfuncs = (mc_cmd_ptr*)realloc(con->cmdfuncs, con->ncmds * sizeof(mc_cmd_ptr));
+	con->cmds = (char**)realloc(con->cmds, con->ncmds * sizeof(char*));
+#else
+	MC_ASSERT(con->ncmds < MC_MAX_COMMANDS);
+	if(con->ncmds == MC_MAX_COMMANDS){
+		return;
+	}
 #endif
 
+	con->cmdfuncs[con->ncmds] = func;
+#ifdef MC_DYNAMIC_ARRAYS
+	con->cmds[con->ncmds] = (char*)malloc(strlen(cmd));
 #endif
+	strcpy(con->cmds[con->ncmds], cmd);
+
+	con->ncmds++;
+}
+
+void mc_input_key(struct mc_console *con, enum mc_keys key)
+{
+	MC_ASSERT(con);
+	switch(key){
+		case MC_KEY_LEFT:
+			if(con->inpos > 0){
+				con->inpos--;
+			}
+			break;
+		case MC_KEY_RIGHT:
+			if(con->instr[con->inpos] != '\0'){
+				con->inpos++;
+			}
+			break;
+		case MC_KEY_UP:
+//TODO
+			break;
+		case MC_KEY_DOWN:
+//TODO
+			break;
+		case MC_KEY_BACKSPACE:
+			if(con->inpos > 0){
+				con->inpos--;
+				memmove(con->instr + con->inpos, con->instr + con->inpos + 1, strlen(con->instr) - con->inpos);
+			}
+			break;
+	}
+}
+
+void mc_input_char(struct mc_console *con, char key)
+{
+	MC_ASSERT(con);
+	if(key >= ' ' && key <= '~'){
+
+	}
+
+	switch(key){
+		case '\t':
+
+			break;
+	}
+}
+
+#ifdef MC_OUTPUT_TEXTURE
+void mc_set_texture_size(struct mc_console *con, unsigned width, unsigned height)
+{
+	MC_ASSERT(con);
+	if(con->pixels){
+		con->pixels = (struct mc_pixel*)realloc(con->pixels, width * height * sizeof(struct mc_pixel));
+	}else{
+		con->pixels = (struct mc_pixel*)malloc(width * height * sizeof(struct mc_pixel));
+	}
+
+	con->width = width;
+	con->height = height;
+}
+#endif // MC_OUTPUT_TEXTURE
+
+#endif // MC_IMPLEMENTATION
+
+#endif // MC_H
